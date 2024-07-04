298a0d210201f232ccebb0d512eb007efc805106
Axel Muñoz <axelg.munoz@hotmail.com>
2024-07-03 23:54:11 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Context from './Context.jsx'
+import Context from './Context'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Context>
